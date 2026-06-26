862a77a183f07bdd2fb5afd412d47c47e2cc81aa
FJLorenzon <kco_lorenzon@live.com.ar>
2026-06-26 01:31:04 -0300

    vamo bien

---
+++ src/db.py
@@ -0,0 +1,65 @@
+'''
+Created on 25 jun 2026
+
+@author: ati05
+'''
+
+import sqlite3
+
+conexion = sqlite3.connect("datos_penales.db")
+cursor = conexion.cursor()
+
+def crear_tablas():
+
+    cursor.execute("""
+        CREATE TABLE IF NOT EXISTS jugadores(
+            id_jugador INTEGER PRIMARY KEY AUTOINCREMENT,
+            nombre TEXT NOT NULL,
+            pierna TEXT NOT NULL
+        )
+    """)
+
+    cursor.execute("""
+        CREATE TABLE IF NOT EXISTS penales(
+            id_penal INTEGER PRIMARY KEY AUTOINCREMENT,
+            id_jugador INTEGER NOT NULL,
+            partido TEXT NOT NULL,
+            posicion TEXT NOT NULL,
+            FOREIGN KEY(id_jugador)
+            REFERENCES jugadores(id_jugador)
+        )
+    """)
+
+    conexion.commit()
+
+
+### DEFINIMOS LOS INSERTS
+#--------------------------------------------------
+def insertar_jugador(nombre, pierna):
+    cursor.execute(
+        "INSERT INTO jugadores(nombre, pierna) VALUES (?, ?)",
+        (nombre, pierna)
+    )
+    conexion.commit()
+    
+def insertar_penal(id_jugador, partido, disparo):
+    cursor.execute(
+        "INSERT INTO penales (id_jugador, partido, disparo) VALUES (?,?,?)", 
+        (id_jugador, partido, disparo)
+    )
+    conexion.commit()
+    
+def select_jugadores():
+    cursor.execute("SELECT * FROM jugadores ORDER BY nombre")
+    return cursor.fetchall()
+    
+def select_penales(id_jugador):
+    cursor.execute("SELECT * FROM penales WHERE id_jugador = ?", (id_jugador))
+    return cursor.fetchall()
+    
+    
+    
+    
+    
+    
+
